667eaf014cb313cc14203db9ea5a9f065d296a86
CupoTa <superlandpage@gmail.com>
2023-03-07 18:51:43 +0300

    strict mode

--- src/index.js
+++ src/index.js
@@ -13,21 +13,24 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <WagmiConfig client={client}>
-            <App />
-            <ToastContainer
-                position="bottom-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-        </WagmiConfig>
-    </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter>
+            <WagmiConfig client={client}>
+                <App />
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </WagmiConfig>
+        </BrowserRouter>
+    </React.StrictMode>
+
 );
 
